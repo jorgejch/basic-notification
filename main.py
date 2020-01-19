75b960f869f973a83fd0ea4625058698529353d7
Jorge Haddad <jorge.haddad@sonos.com>
2020-01-19 06:10:04 -0800

    Implement email notification

--- main.py
+++ main.py
@@ -5,27 +5,53 @@ import os
 from datetime import datetime
 
 import pytz
+import smsutil
 from google.cloud import error_reporting
 from twilio.base.exceptions import TwilioException
 from twilio.rest import Client as TwilioClient
 from google.cloud import storage
-# from google.cloud.logging import Client as LoggingClient
 from google.cloud import exceptions as google_cloud_exceptions
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
+_vars_dict = None
+_error_reporting_client = None
+_logger = None
 
 
-def get_logger(log_level: str):
-    # client = LoggingClient()
-    # handler = client.get_default_handler()
+def get_logger() -> logging.Logger:
+    global _logger
+    if _logger is not None:
+        return _logger
+
     formatter = logging.Formatter('[{levelname:<s}]-[{name:>s}]: {message}', None, '{')
+    stream_handler = logging.StreamHandler()
     logger = logging.getLogger()
+    logger.addHandler(stream_handler)
     for handler in logger.handlers:
         handler.setFormatter(formatter)
-        handler.setLevel(logging._nameToLevel[log_level])
-    # handler = logging.Handler
-    # logger = logging.getLogger('cloudLogger')
-    # logger.addHandler(handler)
-    logger.setLevel(logging._nameToLevel[log_level])
-    return logger
+        handler.setLevel(logging._nameToLevel[os.getenv('LOG_LEVEL')])
+    logger.setLevel(logging._nameToLevel[os.getenv('LOG_LEVEL')])
+    _logger = logger
+    return _logger
+
+
+def get_vars_dict() -> dict:
+    global _vars_dict
+    if _vars_dict is not None:
+        return _vars_dict
+    storage_client = storage.Client()
+    blob = storage_client.get_bucket(os.getenv('VARS_BUCKET')).get_blob(os.getenv('VARS_BLOB'))
+    _vars_dict = json.loads(blob.download_as_string())
+    return _vars_dict
+
+
+def get_error_reporting_client() -> error_reporting.Client:
+    global _error_reporting_client
+    if _error_reporting_client is not None:
+        return _error_reporting_client
+    _error_reporting_client = error_reporting.Client()
+    return _error_reporting_client
 
 
 def get_twilio_client(acct_sid: str, auth_token: str) -> TwilioClient:
@@ -34,73 +60,62 @@ def get_twilio_client(acct_sid: str, auth_token: str) -> TwilioClient:
     return TwilioClient(twilio_account_sid, twilio_auth_token)
 
 
-def get_vars_dict(bucket_name: str, blob_name: str) -> dict:
-    storage_client = storage.Client()
-    blob = storage_client.get_bucket(bucket_name).get_blob(blob_name)
-    return json.loads(blob.download_as_string())
-
-
 def notify_sms(event, context):
     """
     >>> import mock
     >>> import main
-    >>> import os
     >>> mock_context = mock.Mock()
     >>> mock_context.event_id = '617187464135194'
     >>> mock_context.timestamp = '2019-07-15T22:09:03.761Z'
-    >>> data = {}
-    >>> def get_vars_dict(bucket_name, blob_name):
-    ...     return {
-    ...                 "TWILIO_ACCOUNT_SID": os.getenv('TWILIO_ACCOUNT_SID'),
-    ...                 "TWILIO_AUTH_TOKEN": os.getenv('TWILIO_AUTH_TOKEN'),
-    ...                 "TWILIO_NOTIFY_SID": os.getenv('TWILIO_NOTIFY_SID'),
-    ...            }
-    >>> main.get_vars_dict = get_vars_dict
-    >>> main.notify_sms(data, mock_context)
+    >>> mock_context.resource = 'scan_subreddits_new'
+    >>> payload = {'sms': {'to_numbers': ['+18052845139'],'message': 'Hello nurse!!!'}}
+    >>> event = {'data': base64.b64encode(bytes(json.dumps(payload), 'utf-8'))}
+    >>> main.notify_sms(event, mock_context)
     0
 
-    :param event:
-    :param context:
-    :return:
+    :param event: dict The `data` field contains the PubsubMessage message. The `attributes` field will contain custom
+     attributes if there are any.
+    :param context: google.cloud.functions.Context The `event_id` field contains the Pub/Sub message ID. The `timestamp`
+     field contains the publish time. The `event_type` field is the type of the event,
+     ex: "google.pubsub.topic.publish". The `resource` field is the resource that emitted the event.
+    :return: 0 | 1
     """
-    error_reporting_client = error_reporting.Client()
-    logger = get_logger(os.getenv('LOG_LEVEL'))
+    get_logger().info("Received request to notify via sms. Event id: {}. Emitting resource: {}.".format(
+        context.event_id, context.resource
+    ))
 
     try:
-        vars_dict = get_vars_dict(os.getenv('VARS_BUCKET'), os.getenv('VARS_BLOB'))
-    except google_cloud_exceptions.NotFound as e:
-        logger.error("Failed to obtain vars dictionary due to: {}.".format(e))
-        error_reporting_client.report_exception()
+        from_number = get_vars_dict()['FROM_SMS_NUMBER']
+    except KeyError as ke:
+        get_logger().error("Failed to get from sms number variable(s) due to: {}.".format(ke))
+        get_error_reporting_client().report_exception()
         return 1
-
-    try:
-        from_number = vars_dict['FROM_SMS_NUMBER']
-    except KeyError as e:
-        logger.error("Failed to get from sms number variable(s) due to: {}.".format(e))
-        error_reporting_client.report_exception()
+    except google_cloud_exceptions.NotFound as nfe:
+        get_logger().error("Failed to get from sms number variable(s) due to: {}.".format(nfe))
+        get_error_reporting_client().report_exception()
         return 1
 
     try:
         sms_data = json.loads(base64.b64decode(event['data']).decode('utf-8'))['sms']
-        message_text = sms_data['message']
+        message_text = smsutil.encode(sms_data['message'])
         to_numbers = sms_data['to_numbers']
     except KeyError as e:
-        logger.error("Message not found in event['data'] due to: {}.".format(e))
-        error_reporting_client.report_exception()
+        get_logger().error("Message not found in event['data'] due to: {}.".format(e))
+        get_error_reporting_client().report_exception()
         return 1
 
     try:
         for to_num in to_numbers:
             message = get_twilio_client(
-                vars_dict['TWILIO_ACCOUNT_SID'],
-                vars_dict['TWILIO_AUTH_TOKEN']
+                get_vars_dict()['TWILIO_ACCOUNT_SID'],
+                get_vars_dict()['TWILIO_AUTH_TOKEN']
             ).messages.create(
                 from_=from_number,
                 to=to_num,
                 body=message_text
             )
 
-            logger.info(
+            get_logger().info(
                 "Message sent at {} with notification sid {} to number {} and with body '{}'.".format(
                     datetime.now(tz=pytz.timezone('US/Pacific')).timestamp(), message.sid, to_num, message_text
                 )
@@ -109,10 +124,79 @@ def notify_sms(event, context):
         logging.error("Failed to send sms messages with body {} to numbers {} due to: {}.".format(
             message_text, to_numbers, e
         ))
+        get_error_reporting_client().report_exception()
         return 1
-
     return 0
 
 
 def notify_email(event, context):
-    pass
+    """
+    >>> import main
+    >>> import mock
+    >>> mock_context = mock.Mock()
+    >>> mock_context.event_id = '617187464135194'
+    >>> mock_context.timestamp = '2019-07-15T22:09:03.761Z'
+    >>> payload = {'email':
+    ...             {
+    ...                 'message': 'Hello nurse!!!',
+    ...                 'to_emails': ['jorgejch@gmail.com', 'jorgejchaddad@yahoo.com'],
+    ...                 'subject': 'Test emails.'
+    ...             }
+    ...         }
+    >>> event = {'data': base64.b64encode(bytes(json.dumps(payload), 'utf-8'))}
+    >>> main.notify_email(event, mock_context)
+    0
+
+    :param event: dict The `data` field contains the PubsubMessage message. The `attributes` field will contain custom
+     attributes if there are any.
+    :param context: google.cloud.functions.Context The `event_id` field contains the Pub/Sub message ID. The `timestamp`
+     field contains the publish time. The `event_type` field is the type of the event,
+     ex: "google.pubsub.topic.publish". The `resource` field is the resource that emitted the event.
+    :return: 0 | 1
+    """
+    get_logger().info("Received request to notify via email. Event id: {}. Emitting resource: {}.".format(
+        context.event_id, context.resource
+    ))
+
+    try:
+        email_data = json.loads(base64.b64decode(event['data']).decode('utf-8'))['email']
+        message_content = email_data['message']
+        to_emails = email_data['to_emails']
+        subject = email_data['subject']
+    except KeyError as e:
+        get_logger().error("Message not found in event['data'] due to: {}.".format(e))
+        get_error_reporting_client().report_exception()
+        return 1
+
+    try:
+        from_email = get_vars_dict()['FROM_EMAIL']
+    except Exception as e:
+        get_logger().error('Unable to get from email due to: {}.'.format(e))
+        get_error_reporting_client().report_exception()
+        return 1
+
+    message = Mail(
+        from_email=from_email,
+        to_emails=to_emails,
+        subject=subject,
+        html_content=message_content
+    )
+
+    try:
+        sg = SendGridAPIClient(get_vars_dict()['SENDGRID_API_KEY'])
+        response = sg.send(message)
+    except google_cloud_exceptions.NotFound as nfe:
+        get_logger().error("Failed to get sendgrid api key to send email message due to: {}.".format(nfe))
+        get_error_reporting_client().report_exception()
+        return 1
+    except Exception as e:
+        get_logger().error("Failed to send email message due to: {}.".format(e))
+        get_error_reporting_client().report_exception()
+        return 1
+
+    get_logger().info("Sent email to '{}' from '{}' with subject '{}'.".format(to_emails, from_email, subject))
+    get_logger().debug("Sent email content:\n{}".format(message_content))
+    get_logger().debug('Response status code: {}.'.format(response.status_code))
+    get_logger().debug('Response body: {}.'.format(response.body))
+    get_logger().debug('Response headers:\n{}'.format(response.headers))
+    return 0
